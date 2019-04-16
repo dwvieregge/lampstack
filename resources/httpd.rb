96910efd234d9be4bad71820744ba83ce15e623e
dwvieregge <dvieregge@zmotauto.com>
2019-04-16 15:13:21 -0700

    test

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -3,7 +3,7 @@ resource_name :httpd
 action :create do
     execute "httpd-install" do
         command "sudo yum install -y httpd"
-        action :run
+        action [:start, :restart]
         ##not_if { ::File.exist?('/var/website/command.txt') }
   end
 
